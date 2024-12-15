ea75647c10a568f461f0da8bc2aace5a4c568952
NOLOVE954048 <shalukumari954048@gmail.com>
2024-12-15 17:48:49 +0530

    Add files via upload

---
+++ Vip.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Vip_Ddos_07
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7365486186:AAFMKkpIIHZin56W8vp_NF71cUX3Zi_L-Zw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@@BOT_HU_ME_BHAI")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6107347121")
+
+
+
+
+
+  
